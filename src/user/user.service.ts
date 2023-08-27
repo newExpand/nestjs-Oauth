faa2686ae201ab8823e6bdcbb29bb8e883e29586
newExpand <magin0auto@gmail.com>
2023-08-27 12:12:06 +0900

    controller에 맞게 service 코드 작성

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,4 +8,32 @@ export class UserService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
+  createUser(user): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string) {
+    const result = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    return result;
+  }
+
+  async updateUser(email, _user) {
+    const user: User = await this.getUser(email);
+    console.log(_user);
+
+    user.username = _user.username;
+    user.password = _user.password;
+
+    console.log(user);
+
+    this.userRepository.save(user);
+  }
+
+  deleteUser(email) {
+    return this.userRepository.delete({ email });
+  }
 }
