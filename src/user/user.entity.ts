f091c37b282d1983b8487dd7add6b3d06f9c619e
newExpand <magin0auto@gmail.com>
2023-09-05 13:49:28 +0900

    구글 Oauth로 들어오는 데이터에 맞게 User entity 파일 코드 추가

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -10,13 +10,16 @@ export class User {
   @Column({ unique: true })
   email: string;
 
-  @Column()
+  @Column({ nullable: true })
   password: string;
 
   @Column()
   username: string;
 
   // 생성일 default: true로 기본값 설정
-  @Column({ default: true })
+  @Column({ type: 'datetime', default: 'CURRENT_TIMESTAMP' })
   createdDt: Date = new Date();
+
+  @Column({ nullable: true })
+  providerId: string;
 }
